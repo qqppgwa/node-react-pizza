80dce92e4ad5a90ecbad293d5a94a2be569369fd
qqppgwa <qqppgwa@gmail.com>
2019-12-07 20:33:42 +0800

    ..

--- app.js
+++ app.js
@@ -1,84 +1,81 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var mysql = require("mysql");
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+var mysql = require('mysql')
 // console.log(mysql)
-var app = express();
+var app = express()
 var dbinfo = {
-  host: "us-cdbr-iron-east-05.cleardb.net",
-  user: "b4691cf11c462d",
-  password: "76c6ca09",
-  database: "heroku_e1f59cb6f92f50c"
-};
+  host: 'us-cdbr-iron-east-05.cleardb.net',
+  user: 'b4691cf11c462d',
+  password: '76c6ca09',
+  database: 'heroku_e1f59cb6f92f50c'
+}
 var con
 
 function handleErr() {
   con = mysql.createConnection(dbinfo)
 
-
   console.log(con)
-  con.connect(function (err) {
+  con.connect(function(err) {
     if (err) {
-      console.log('connecting error');
+      console.log('connecting error')
       setTimeout(handleErr, 2000)
       // return;
     }
-    console.log('connecting success');
-  });
-  con.on('error', function (err) {
-    console.log('db error', err);
+    console.log('connecting success')
+  })
+  con.on('error', function(err) {
+    console.log('db error', err)
     // 如果是连接断开，自动重新连接
     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleErr();
+      handleErr()
     } else {
-      throw err;
+      throw err
     }
-  });
-
-
+  })
 }
 handleErr()
-app.use(function (req, res, next) {
-  req.con = con;
-  next();
-});
+app.use(function(req, res, next) {
+  req.con = con
+  next()
+})
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+)
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+app.use(function(req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-var port = process.env.PORT || 3000;
-app.listen(port);
-module.exports = app;
+  res.status(err.status || 500)
+  res.render('error')
+})
+var port = process.env.PORT || 3000
+app.listen(port)
+module.exports = app
