939b14149aa185ea384141a0032ffcaa2f33ba02
qqppgwa <qqppgwa@gmail.com>
2019-12-07 21:50:15 +0800

    ..

---
+++ react-ui/build/precache-manifest.3ce8bfcde68a37b66f73926c70d93407.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6afc2dbb06f5caa592cd5124044e1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a7208a796b0a488bdf8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0ad4a605ecc9d649f5bb",
+    "url": "/static/js/2.8eb07931.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8eb07931.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a7208a796b0a488bdf8",
+    "url": "/static/js/main.9895b0fa.chunk.js"
+  },
+  {
+    "revision": "e55d3ce97165bcf00d17",
+    "url": "/static/js/runtime-main.45dee6ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
